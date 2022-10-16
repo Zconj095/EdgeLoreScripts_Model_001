4193ab6fe2c804451b828f37d48c0ef21363e108
Zconj095 <96359501+Zconj095@users.noreply.github.com>
2022-10-16 17:04:38 -0400

    Add files via upload

---
+++ Creature_Stats.py
@@ -0,0 +1,35 @@
+CREATURESTATS = [0, 1, 2, 3, 4, 5]
+ATTACKSTAT = "ATTACKSTAT"
+DEFENSESTAT = "DEFENSESTAT"
+SPECIALDEFENSESTAT = "SPECIALDEFENSESTAT"
+SPECIALATTACKSTAT = "SPECIALATTACKSTAT"
+SPEEDSTAT = "SPEEDSTAT"
+
+speedstatname = CREATURESTATS[0]
+speedstatvalue = pokemondimensionvaluename = SPEEDSTAT
+speedstatname = speedstatvalue
+speedstat = speedstatname = speedstatvalue
+
+
+specialattackstatname = CREATURESTATS[1]
+specialattackstatvalue = creaturestatsname = SPECIALDEFENSESTAT
+specialattackstatname = specialattackstatvalue
+specialattackstat = specialattackstatname = specialattackstatvalue
+
+
+defensestatname = CREATURESTATS[2]
+defensestatvalue = creaturestatsname = DEFENSESTAT
+defensestatname = defensestatvalue
+defensestat = defensestatname = defensestatvalue
+
+
+specialdefensestatname = CREATURESTATS[3]
+specialdefensestatvalue = creaturestatsname = SPECIALATTACKSTAT
+specialdefensestatname = specialdefensestatvalue
+specialdefensestat = specialdefensestatname = specialdefensestatvalue
+
+
+attackstatname = CREATURESTATS[4]
+attackstatvalue = creaturestatsname = ATTACKSTAT
+attackstatname = attackstatvalue
+attackstat = attackstatname = attackstatvalue
